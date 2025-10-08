f3d70788aa1ed7c15e4f3eb383ae23d574d68fc9
dangtai07 <113095335+dangtai07@users.noreply.github.com>
2025-10-08 15:26:49 +0700

    EX2
    EX2

--- main.c
+++ main.c
@@ -95,15 +95,22 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+	     HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_SET);
+	     HAL_GPIO_WritePin(LED_YEL_GPIO_Port, LED_YEL_Pin, GPIO_PIN_RESET);
+	     HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+	     HAL_Delay(5000);
 
-	    HAL_GPIO_WritePin(RED_LED_GPIO_Port, RED_LED_Pin, GPIO_PIN_SET);
-	    HAL_GPIO_WritePin(YEL_LED_GPIO_Port, YEL_LED_Pin, GPIO_PIN_RESET);
-	    HAL_Delay(2000);
+	     // YELLOW phase (2s)
+	     HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+	     HAL_GPIO_WritePin(LED_YEL_GPIO_Port, LED_YEL_Pin, GPIO_PIN_SET);
+	     HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_RESET);
+	     HAL_Delay(2000);
 
-	    HAL_GPIO_WritePin(RED_LED_GPIO_Port, RED_LED_Pin, GPIO_PIN_RESET);
-	    HAL_GPIO_WritePin(YEL_LED_GPIO_Port, YEL_LED_Pin, GPIO_PIN_SET);
-	    HAL_Delay(2000);
-
+	     // GREEN phase (3s)
+	     HAL_GPIO_WritePin(LED_RED_GPIO_Port, LED_RED_Pin, GPIO_PIN_RESET);
+	     HAL_GPIO_WritePin(LED_YEL_GPIO_Port, LED_YEL_Pin, GPIO_PIN_RESET);
+	     HAL_GPIO_WritePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin, GPIO_PIN_SET);
+	     HAL_Delay(3000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -161,10 +168,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, RED_LED_Pin|YEL_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YEL_Pin|LED_GREEN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : RED_LED_Pin YEL_LED_Pin */
-  GPIO_InitStruct.Pin = RED_LED_Pin|YEL_LED_Pin;
+  /*Configure GPIO pins : LED_RED_Pin LED_YEL_Pin LED_GREEN_Pin */
+  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YEL_Pin|LED_GREEN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
